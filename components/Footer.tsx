9d49a2a063ef1d43b089a5b2a9c1667a34a2959a
Kimberly Druist <kimberly@example.com>
2026-04-18 22:26:43 -0700

    feat(components): scaffold Footer

---
+++ components/Footer.tsx
@@ -0,0 +1,8 @@
+export function Footer() {
+  return (
+    <footer className="mx-auto flex w-full max-w-copy flex-col gap-2 px-6 pb-10 pt-16 text-sm text-foreground/75 md:px-8">
+      <p>&copy; 2026 Kimberly Druist</p>
+      <p>Last updated: TK</p>
+    </footer>
+  );
+}
